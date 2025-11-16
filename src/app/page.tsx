76dc9ac6ba417d1af812d19449ff4719d90adf7b
TheComing06 <t.abdrahmanov999@gmail.com>
2025-11-16 17:25:30 +0700

    fix map center position and zoom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         src={`https://api-maps.yandex.ru/v3/?apikey=${process.env.YANDEX_API_KEY}&lang=ru_RU`}
         strategy="beforeInteractive"
       />
-      <div className="h-150 w-300 flex flex-row items-center justify-center">
+      <div className="h-150 w-300 flex flex-row items-center justify-center mx-auto my-8">
         <Map />
       </div>
     </>
--- variables.ts
+++ variables.ts
@@ -1,6 +1,6 @@
 import type {YMapLocationRequest} from '@yandex/ymaps3-types';
 
 export const LOCATION: YMapLocationRequest = {
-  center: [37.623082, 55.75254], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  center: [82.917839, 55.031095], // starting position [lng, lat]
+  zoom: 12 // starting zoom
 };
