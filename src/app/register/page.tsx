fa9e590d43752a18e8115d37960fed8525900a66
TheComing06 <t.abdrahmanov999@gmail.com>
2025-11-16 20:54:15 +0700

    add simple navigation

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -12,7 +12,7 @@ export default function Register() {
                     <input placeholder="Пароль" type="password" className="shadow rounded-md bg-neutral-200 text-black font-medium text-base w-auto h-auto px-1.5"/>
                     <input placeholder="Пароль повторно" type="password" className="shadow rounded-md bg-neutral-200 text-black font-medium text-base w-auto h-auto px-1.5"/>
                 </div>
-                <button className="text-black rounded-md font-bold bg-neutral-100 shadow hover:bg-neutral-300 px-3.5 py-1.5 transform hover:scale-125 duration-200 transition ease-in-out">Зарегистрироваться</button>
+                <Link href="/auth" className="text-black rounded-md font-bold bg-neutral-100 shadow hover:bg-neutral-300 px-3.5 py-1.5 transform hover:scale-125 duration-200 transition ease-in-out">Зарегистрироваться</Link>
             </div>
         </>
     );
