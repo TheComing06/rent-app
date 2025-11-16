43f9d41afac10e676794b9ae9065b3b123183480
TheComing06 <t.abdrahmanov999@gmail.com>
2025-11-16 20:46:44 +0700

    add front to register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
+
 export default function Register() {
     return (
         <>
-            <div className="text-black">
-                amegalul-register-page
+            <div className="flex flex-col items-center justify-center gap-10">
+                <h1 className="text-black font-extrabold text-2xl mt-10 mb-4">Авторизация</h1>
+                <div className="flex flex-col gap-3 items-center justify-center">
+                    <input placeholder="Логин" type="text" className="shadow rounded-md bg-neutral-200 text-black font-normal text-base w-auto h-auto px-1.5"/>
+                    <input placeholder="Пароль" type="password" className="shadow rounded-md bg-neutral-200 text-black font-normal text-base w-auto h-auto px-1.5"/>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-3.5">
+                    <button className="text-black rounded-md font-bold bg-neutral-100 shadow hover:bg-neutral-300 px-3.5 py-1.5 transform hover:scale-125 duration-200 transition ease-in-out">Авторизоваться</button>
+                    <Link href="/register" className="text-gray-500 hover:text-gray-700 text-sm">Зарегистрироваться</Link>
+                </div> 
             </div>
         </>
     );
