7c769860925f924a27e3d5796a34568a5d176380
TheComing06 <t.abdrahmanov999@gmail.com>
2025-11-19 13:33:16 +0700

    add simple design for cabinet page

--- src/app/cabinet/page.tsx
+++ src/app/cabinet/page.tsx
@@ -1,8 +1,22 @@
+import profileIcon from "@/../public/images/profile.svg";
+import Image from "next/image";
+
 export default function Cabinet() {
     return (
         <>
-            <div className="text-black">
-                cabinet-page
+            <div className="text-black flex flex-col items-center justify-center gap-2">
+                <h1 className="font-extrabold text-2xl mt-10 mb-4">Личный кабинет</h1>
+                <div className="flex flex-row gap-50">
+                    <div className="flex flex-row items-center justify-center gap-1.5 my-2">
+                        <Image src={profileIcon} alt="" className="scale-125"/>
+                        Логин: login
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <h1 className="font-bold">Информация о профиле</h1>
+                        <div>Почта: pochta</div>
+                        <div>Телефон: nomer</div>
+                    </div>
+                </div>
             </div>
         </>
     );
