d1fd1ac4b0634c2eba273923186f8c19589ced36
TheComing06 <t.abdrahmanov999@gmail.com>
2025-11-19 17:57:50 +0700

    fix maxLength for input in register and add mask library

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import React from "react";
+import InputMask from "react-input-mask";
 
 export default function Register() {
   return (
@@ -16,31 +18,37 @@ export default function Register() {
           <input
             placeholder="Имя"
             type="text"
+            maxLength={100}
             className="shadow rounded-md bg-neutral-200 text-black font-medium text-base w-auto h-auto px-1.5"
           />
           <input
             type="text"
             placeholder="Фамилия"
+            maxLength={100}
             className="shadow rounded-md bg-neutral-200 text-black font-medium text-base w-auto h-auto px-1.5"
           />
           <input
             type="text"
             placeholder="Отчество"
+            maxLength={100}
             className="shadow rounded-md bg-neutral-200 text-black font-medium text-base w-auto h-auto px-1.5"
           />
           <input
             placeholder="Почта"
             type="text"
+            maxLength={100}
             className="shadow rounded-md bg-neutral-200 text-black font-medium text-base w-auto h-auto px-1.5"
           />
           <input
             placeholder="Телефон"
             type="text"
+            maxLength={30}
             className="shadow rounded-md bg-neutral-200 text-black font-medium text-base w-auto h-auto px-1.5"
           />
           <input
             placeholder="Пароль"
             type="password"
+            maxLength={20}
             className="shadow rounded-md bg-neutral-200 text-black font-medium text-base w-auto h-auto px-1.5"
           />
           <input
